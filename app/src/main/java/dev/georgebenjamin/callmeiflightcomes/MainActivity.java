f1e436d1e27f46d244c98de88599042390c1b5d3
georgeben <msdcconnect@gmail.com>
2019-03-29 09:09:14 +0100

    Add phone number edit text

--- app/src/main/java/dev/georgebenjamin/callmeiflightcomes/MainActivity.java
+++ app/src/main/java/dev/georgebenjamin/callmeiflightcomes/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     private PackageManager mPackageManager;
     private ComponentName mComponentName;
     private static final int CALL_PHONE_PERMISSION_REQ_CODE = 1;
+    
+    private EditText phoneNumEditText;
+    private TextView mLightStatus;
+    private String mNumberToCall = "09060791349";
 
     //This holds a reference to the main activity
     private static MainActivity instance;
@@ -31,7 +38,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    
+        phoneNumEditText = (EditText) findViewById(R.id.phoneNumber);
+        mLightStatus = (TextView) findViewById(R.id.lightStatus);
+        
         mPackageManager = getPackageManager();
         mComponentName = new ComponentName(this, PowerReceiver.class);
 
@@ -67,13 +77,15 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 //Value returned from Power receiver => 0 = not charging, 1 = charging
                 if(value == 0){
-                    Toast.makeText(MainActivity.this, "They have taken light", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, R.string.no_light, Toast.LENGTH_SHORT).show();
+                    mLightStatus.setText(R.string.no_light);
 
                 }else{
                     Toast.makeText(MainActivity.this, "They have brought light", Toast.LENGTH_SHORT).show();
+                    mLightStatus.setText(R.string.theres_light);
                     //Make phone call
                     if(checkForPermission(Manifest.permission.CALL_PHONE)){
-                        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:09060791349")));
+                        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+mNumberToCall)));
                     }else{
                         Toast.makeText(MainActivity.this, "Call permission not grated", Toast.LENGTH_SHORT).show();
                     }
@@ -94,6 +106,21 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     Toast.makeText(MainActivity.this, "Call permission granted", Toast.LENGTH_SHORT).show();
                 }
+        }
+    }
+
+    public void savePhoneNumber(View view) {
+        String phoneNumber = phoneNumEditText.getText().toString().trim();
+        if(!TextUtils.isEmpty(phoneNumber)){
+            if(phoneNumber.length() < 11){
+                Toast.makeText(MainActivity.this, "Please enter a valid phone number", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mNumberToCall = phoneNumber;
+            Toast.makeText(this, "Phone number added successfully", Toast.LENGTH_SHORT).show();
+            phoneNumEditText.setText("");
+        }else{
+            Toast.makeText(instance, "Please enter a phone number", Toast.LENGTH_SHORT).show();
         }
     }
 }
